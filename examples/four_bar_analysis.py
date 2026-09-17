75a1a2ff6f72fa36b6fab0928bf104f671f82eb2
Pedro Jorge De Los Santos <delossantosmfq@gmail.com>
2026-09-17 17:05:29 -0600

    docs: migrate examples to solve 0.3 API

--- examples/four_bar_analysis.py
+++ examples/four_bar_analysis.py
@@ -50,8 +50,8 @@ def main():
 
     solution = solve(
         mechanism,
-        input=input_joint,
-        values=input_angle,
+        input_joint=input_joint,
+        input_position=input_angle,
         input_velocity=input_angular_velocity,
         input_acceleration=input_angular_acceleration,
         initial_guess=initial_guess,

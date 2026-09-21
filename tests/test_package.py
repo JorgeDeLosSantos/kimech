0bbc1d9e6cd75564bfcc746624ec89bdef8c9aaf
Pedro Jorge De Los Santos <delossantosmfq@gmail.com>
2026-09-21 16:46:26 -0600

    test: pin expected release version to 0.4.0

--- tests/test_package.py
+++ tests/test_package.py
@@ -7,5 +7,5 @@ def test_runtime_version_matches_distribution_metadata():
     assert kimech.__version__ == version("kimech")
 
 
-def test_release_version_is_0_3_0():
-    assert kimech.__version__ == "0.3.0"
+def test_release_version_is_0_4_0():
+    assert kimech.__version__ == "0.4.0"

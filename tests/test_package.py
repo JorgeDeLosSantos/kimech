17b7b08833200b797106d3c1b58663707f262fc4
Pedro Jorge De Los Santos <delossantosmfq@gmail.com>
2026-09-24 16:20:19 -0600

    test: expect 0.5.0 release version

--- tests/test_package.py
+++ tests/test_package.py
@@ -7,5 +7,5 @@ def test_runtime_version_matches_distribution_metadata():
     assert kimech.__version__ == version("kimech")
 
 
-def test_release_version_is_0_4_0():
-    assert kimech.__version__ == "0.4.0"
+def test_release_version_is_0_5_0():
+    assert kimech.__version__ == "0.5.0"
